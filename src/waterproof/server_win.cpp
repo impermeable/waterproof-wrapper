f54de758e4d2cdd174e89640a40210594ac23cfe
davidot <david.tuin@gmail.com>
2022-10-21 15:08:16 +0200

    Windows: Fix import order in server_win for MSVC builds

--- src/waterproof/server_win.cpp
+++ src/waterproof/server_win.cpp
@@ -14,8 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#include <inaddr.h>
 #include "server.h"
+#include <inaddr.h>
 
 namespace wpwrapper {
 
